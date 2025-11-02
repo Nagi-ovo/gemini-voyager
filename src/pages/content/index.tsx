5eae5be1cd0f3ae45146474ca5848f5f79f1c717
Nagi-ovo <13264500190@163.com>
2025-11-02 13:52:25 +0000

    chore: run eslint

--- src/pages/content/index.tsx
+++ src/pages/content/index.tsx
@@ -1,7 +1,7 @@
 import { startExportButton } from './export/index';
+import { startFolderManager } from './folder/index';
 import { startPromptManager } from './prompt/index';
 import { startTimeline } from './timeline/index';
-import { startFolderManager } from './folder/index';
 
 try {
   if (location.hostname === 'gemini.google.com') {
