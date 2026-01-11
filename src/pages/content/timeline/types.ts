e30b3e08b054338737dcef3dbb91b642d770210e
shong <yohjishong0523@gmail.com>
2026-01-11 13:00:23 +0800

    feat(timeline): add MarkerLevel function

--- src/pages/content/timeline/types.ts
+++ src/pages/content/timeline/types.ts
@@ -1,1 +1,17 @@
-export type DotElement = HTMLButtonElement & { dataset: DOMStringMap & { targetTurnId?: string } };
+export type DotElement = HTMLButtonElement & { 
+    dataset: DOMStringMap & { 
+        targetTurnId?: string; markerIndex?: string 
+    } 
+};
+
+export type MarkerLevel = 1 | 2 | 3 | 4;
+
+export interface MarkerLevelsData {
+  [conversationId: string]: {
+    [turnId: string]: MarkerLevel;
+  };
+}
+
+export interface CollapsedMarkersData {
+  [conversationId: string]: string[];
+}
