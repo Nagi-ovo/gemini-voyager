c0b8f3e0f1d5efc8fdc77d0dfd6d11cc1f073e65
Nagi-ovo <101612750+Nagi-ovo@users.noreply.github.com>
2026-01-25 17:10:39 +0000

    feat(docs): add HomeReviews component with scrolling testimonials

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,11 +1,18 @@
 import { type Theme } from 'vitepress';
 import DefaultTheme from 'vitepress/theme';
+import { h } from 'vue';
 
+import HomeReviews from './components/HomeReviews.vue';
 import './style.css';
 
 export default {
   extends: DefaultTheme,
+  Layout: () => {
+    return h(DefaultTheme.Layout, null, {
+      'home-features-after': () => h(HomeReviews),
+    });
+  },
   enhanceApp({ app }) {
-    // register global components if needed
+    app.component('HomeReviews', HomeReviews);
   },
 } satisfies Theme;
