a1db0250e7b6b90ad2ed6e3a08ca425658edea04
Nagi-ovo <101612750+Nagi-ovo@users.noreply.github.com>
2026-03-02 02:08:48 +0000

    chore: bump to v1.3.0 🎉

--- src/pages/content/changelog/index.ts
+++ src/pages/content/changelog/index.ts
@@ -277,6 +277,16 @@ function createChangelogModal(
   githubLink.innerHTML =
     '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M12 2C6.477 2 2 6.477 2 12c0 4.42 2.865 8.17 6.839 9.49.5.092.682-.217.682-.482 0-.237-.008-.866-.013-1.7-2.782.604-3.369-1.34-3.369-1.34-.454-1.156-1.11-1.464-1.11-1.464-.908-.62.069-.608.069-.608 1.003.07 1.531 1.03 1.531 1.03.892 1.529 2.341 1.087 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.11-4.555-4.943 0-1.091.39-1.984 1.029-2.683-.103-.253-.446-1.27.098-2.647 0 0 .84-.269 2.75 1.025A9.578 9.578 0 0112 6.836c.85.004 1.705.115 2.504.337 1.909-1.294 2.747-1.025 2.747-1.025.546 1.377.203 2.394.1 2.647.64.699 1.028 1.592 1.028 2.683 0 3.842-2.339 4.687-4.566 4.935.359.309.678.919.678 1.852 0 1.336-.012 2.415-.012 2.743 0 .267.18.578.688.48C19.138 20.167 22 16.418 22 12c0-5.523-4.477-10-10-10z"/></svg>';
 
+  // X (Twitter) link
+  const xLink = document.createElement('a');
+  xLink.className = 'gv-changelog-icon-link gv-changelog-icon-x';
+  xLink.href = 'https://x.com/Nag1ovo';
+  xLink.target = '_blank';
+  xLink.rel = 'noopener noreferrer';
+  xLink.setAttribute('aria-label', 'X (Twitter)');
+  xLink.innerHTML =
+    '<svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor"><path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-4.714-6.231-5.401 6.231H2.748l7.73-8.835L1.254 2.25H8.08l4.26 5.632 5.904-5.632zm-1.161 17.52h1.833L7.084 4.126H5.117z"/></svg>';
+
   // Docs link with annotation
   const docsWrapper = document.createElement('div');
   docsWrapper.className = 'gv-changelog-docs-wrapper';
@@ -300,6 +310,7 @@ function createChangelogModal(
 
   iconGroup.appendChild(sponsorLink);
   iconGroup.appendChild(githubLink);
+  iconGroup.appendChild(xLink);
   iconGroup.appendChild(docsWrapper);
 
   const gotItBtn = document.createElement('button');
